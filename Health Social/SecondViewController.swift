39534eb5e4c4e34572c802a0b047514cddced225
Ori Benhamou <ori@benhamou.com>
2015-03-05 14:09:45 -0500

    Initial Commit

---
+++ Health Social/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Health Social
+//
+//  Created by Ori's Air on 3/5/15.
+//  Copyright (c) 2015 OriGen Health. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
