8a3fbf4ce1447f8906f0cfd7450bfbae71da3bd4
Ori Benhamou <ori@benhamou.com>
2015-04-17 12:51:52 -0400

    Added a page view controller which will be an additional profile settings page

---
+++ Health Social/ProfilePageContentViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfilePageContentViewController.swift
+//  Health Social
+//
+//  Created by Ori's Air on 4/17/15.
+//  Copyright (c) 2015 OriGen Health. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePageContentViewController: UIViewController {
+
+    @IBOutlet weak var myProfileLabel: UILabel!
+    
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
